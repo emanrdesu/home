bc070ee4451c9f872da2ae4eba97979dae6902ec
emanrdesu <eperezpozo@knights.ucf.edu>
2025-02-16 14:40:44 -0500

    Format code

--- src/lib/Search.svelte
+++ src/lib/Search.svelte
@@ -1,89 +1,89 @@
 <script>
-  import Icon from "$lib/Icon.svelte";
-  let value = "";
+	import Icon from '$lib/Icon.svelte';
+	let value = '';
 
-  function search() {
-    const search = value.trim();
-    if (search) {
-      value = "";
-      window.location.href = `https://duckduckgo.com?q=${encodeURIComponent(search)}`;
-    }
-  }
+	function search() {
+		const search = value.trim();
+		if (search) {
+			value = '';
+			window.location.href = `https://duckduckgo.com?q=${encodeURIComponent(search)}`;
+		}
+	}
 </script>
 
 <div>
-  <input
-    onkeydown={(e) => {
-      if (e.key == "Enter") {
-        search();
-      }
-    }}
-    name="search"
-    type="text"
-    bind:value
-    placeholder="search the web"
-  />
-  <button
-    onclick={search} 
-    class="-translate-x-[1px] px-1 outline-none focus:ring-2 ring-orange-200"
-    tabindex={value.trim() ? 0 : -1}
-  >
-    <Icon name="search" />
-  </button>
+	<input
+		onkeydown={(e) => {
+			if (e.key == 'Enter') {
+				search();
+			}
+		}}
+		name="search"
+		type="text"
+		bind:value
+		placeholder="search the web"
+	/>
+	<button
+		onclick={search}
+		class="-translate-x-[1px] px-1 ring-orange-200 outline-none focus:ring-2"
+		tabindex={value.trim() ? 0 : -1}
+	>
+		<Icon name="search" />
+	</button>
 </div>
 
 <style>
-  div {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 35px;
-    width: 100vw;
-  }
+	div {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 35px;
+		width: 100vw;
+	}
 
-  input {
-    width: 100%;
-    max-width: 444px;
-    outline: none;
-    font-size: 14px;
-    font-weight: 500;
-    background-color: #53535f;
-    text-align: center;
-    padding: 7px 10px;
-    border: 3px solid transparent;
-    border-top-left-radius: 7px;
-    border-bottom-left-radius: 7px;
-    margin-right: 1px;
-    margin-left: 8px;
-    transition: all 200ms ease;
-  }
+	input {
+		width: 100%;
+		max-width: 444px;
+		outline: none;
+		font-size: 14px;
+		font-weight: 500;
+		background-color: #53535f;
+		text-align: center;
+		padding: 7px 10px;
+		border: 3px solid transparent;
+		border-top-left-radius: 7px;
+		border-bottom-left-radius: 7px;
+		margin-right: 1px;
+		margin-left: 8px;
+		transition: all 200ms ease;
+	}
 
-  input:hover {
-    border: 3px solid rgba(255, 255, 255, 0.16);
-  }
+	input:hover {
+		border: 3px solid rgba(255, 255, 255, 0.16);
+	}
 
-  input:focus {
-    border: 3px solid #966ed3;
-    background-color: var(--base-bg-200);
-  }
+	input:focus {
+		border: 3px solid #966ed3;
+		background-color: var(--base-bg-200);
+	}
 
-  button {
-    flex-grow: 0;
-    height: 115%;
-    width: 34px;
-    border: none;
-    cursor: pointer;
-    background-color: rgba(54, 54, 56, 1);
-    border-top-right-radius: 7px;
-    border-bottom-right-radius: 7px;
-    padding: 8px;
-    display: flex;
-    align-items: center;
-    margin-right: 8px;
-  }
+	button {
+		flex-grow: 0;
+		height: 115%;
+		width: 34px;
+		border: none;
+		cursor: pointer;
+		background-color: rgba(54, 54, 56, 1);
+		border-top-right-radius: 7px;
+		border-bottom-right-radius: 7px;
+		padding: 8px;
+		display: flex;
+		align-items: center;
+		margin-right: 8px;
+	}
 
-  button:active,
-  button:hover {
-    background-color: rgb(99, 24, 105);
-  }
+	button:active,
+	button:hover {
+		background-color: rgb(99, 24, 105);
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,97 +1,96 @@
 <script>
-	import Icon from "$lib/Icon.svelte";
-	import Section from "$lib/Section.svelte";
-	import Search from "$lib/Search.svelte";
+	import Icon from '$lib/Icon.svelte';
+	import Section from '$lib/Section.svelte';
+	import Search from '$lib/Search.svelte';
 
-   const sections = [
-    {
-      name: "chan",
-      color: "#00cc7a",
-      links: [
-        { href: "https://4chan.org/a", text: "/a/" },
-        { href: "https://4chan.org/c", text: "/c/" },
-        { href: "https://4chan.org/g", text: "/g/" },
-        { href: "https://4chan.org/wg", text: "/wg/" },
-        { href: "https://4chan.org/r9k", text: "/r9k/" },
-        { href: "https://4chan.org/pol", text: "/pol/" },
-        { href: "https://4chan.org/fit", text: "/fit/" },
-        { href: "https://4chan.org/wsg", text: "/wsg/" },
-        { href: "https://4chan.org/tv", text: "/tv/" },
-        { href: "https://4chan.org/t", text: "/t/" },
-        { href: "https://lainchan.org/%CE%BB/index.html", text: "/λ/" },
-      ],
-    },
+	const sections = [
+		{
+			name: 'chan',
+			color: '#00cc7a',
+			links: [
+				{ href: 'https://4chan.org/a', text: '/a/' },
+				{ href: 'https://4chan.org/c', text: '/c/' },
+				{ href: 'https://4chan.org/g', text: '/g/' },
+				{ href: 'https://4chan.org/wg', text: '/wg/' },
+				{ href: 'https://4chan.org/r9k', text: '/r9k/' },
+				{ href: 'https://4chan.org/pol', text: '/pol/' },
+				{ href: 'https://4chan.org/fit', text: '/fit/' },
+				{ href: 'https://4chan.org/wsg', text: '/wsg/' },
+				{ href: 'https://4chan.org/tv', text: '/tv/' },
+				{ href: 'https://4chan.org/t', text: '/t/' },
+				{ href: 'https://lainchan.org/%CE%BB/index.html', text: '/λ/' }
+			]
+		},
 
-    {
-      name: "person",
-      color: "#93c5fd",
-      links: [
-        { href: "https://app.dataannotation.tech/users/sign_in", text: "work" },
-        { href: "https://nelnet.studentaid.gov/account/login", text: "loan" },
-        { href: "http://192.168.0.1", text: "router" },
-      ],
-    },
+		{
+			name: 'person',
+			color: '#93c5fd',
+			links: [
+				{ href: 'https://app.dataannotation.tech/users/sign_in', text: 'work' },
+				{ href: 'https://nelnet.studentaid.gov/account/login', text: 'loan' },
+				{ href: 'http://192.168.0.1', text: 'router' }
+			]
+		},
 
-    {
-      name: "ai",
-      color: "#a5a4db",
-      links: [
-			  { href: "https://chat.openai.com", text: "chatgpt" },
-			  { href: "https://gemini.google.com", text: "gemini" },
-			  { href: "https://claude.ai", text: "claude" },
-			  { href: "https://chat.deepseek.com", text: "deepseek" },
-      ],
-    },
+		{
+			name: 'ai',
+			color: '#a5a4db',
+			links: [
+				{ href: 'https://chat.openai.com', text: 'chatgpt' },
+				{ href: 'https://gemini.google.com', text: 'gemini' },
+				{ href: 'https://claude.ai', text: 'claude' },
+				{ href: 'https://chat.deepseek.com', text: 'deepseek' }
+			]
+		},
 
-    {
-      name: "anon",
-      color: "#f0a8d0",
-      links: [
-        { href: "https://xvideos.com", text: "xvideos" },
-        { href: "https://4chan.org/gif", text: "/gif/" },
-        { href: "https://4chan.org/s", text: "/s/" },
-      ],
-    },
+		{
+			name: 'anon',
+			color: '#f0a8d0',
+			links: [
+				{ href: 'https://xvideos.com', text: 'xvideos' },
+				{ href: 'https://4chan.org/gif', text: '/gif/' },
+				{ href: 'https://4chan.org/s', text: '/s/' }
+			]
+		},
 
-    {
-      name: "0x40",
-      color: "#85c742",
-      links: [
-        { href: "https://hues.kepstin.ca", text: "hues" },
-        { href: "https://spook.mon.im/", text: "spook" },
-        { href: "https://0x40.mon.im/", text: "mon" },
-        { href: "https://420.mon.im/snoop.html", text: "420" },
-      ],
-    },
+		{
+			name: '0x40',
+			color: '#85c742',
+			links: [
+				{ href: 'https://hues.kepstin.ca', text: 'hues' },
+				{ href: 'https://spook.mon.im/', text: 'spook' },
+				{ href: 'https://0x40.mon.im/', text: 'mon' },
+				{ href: 'https://420.mon.im/snoop.html', text: '420' }
+			]
+		}
+	];
 
-   ]
+	const icons = [
+		{ name: 'whatsapp', color: '#25D366', link: 'https://web.whatsapp.com' },
+		{ name: 'mail', color: '#df9f9f', link: 'https://mail.cock.li' },
+		{ name: 'bank', color: '#1680e9', link: 'https://chase.com' }
+	];
 
-  const icons = [
-		{ name: 'whatsapp', color: '#25D366', link: "https://web.whatsapp.com"  },
-		{ name: 'mail', color: '#df9f9f', link: "https://mail.cock.li" },
-		{ name: 'bank', color: '#1680e9', link: "https://chase.com" }
-	]
-
-	const titles = [ "foobar", "quxpom", "barbaz" ]
+	const titles = ['foobar', 'quxpom', 'barbaz'];
 </script>
 
 <svelte:head>
 	<title>{titles[Math.round(Math.random() * 2)]}</title>
 </svelte:head>
 
-<div class="hero justify-center text-center flex flex-col">
- {#if navigator.onLine}
-	<small class="fixed right-3 bottom-2 italic">
-	  {#await fetch("http://ifconfig.me/ip").then((r) => r.text()) then ip}
-		 {ip}
-	  {/await}
-	</small>
- {/if}
+<div class="hero flex flex-col justify-center text-center">
+	{#if navigator.onLine}
+		<small class="fixed right-3 bottom-2 italic">
+			{#await fetch('http://ifconfig.me/ip').then((r) => r.text()) then ip}
+				{ip}
+			{/await}
+		</small>
+	{/if}
 
-	<div class="flex flex-wrap gap-8 mb-6">
-		{#each icons as { name, color, link } }
-			<a class="oscilla rounded-lg outline-none focus:ring-2 ring-orange-200" href={link}>
-				<Icon {name}/>
+	<div class="mb-6 flex flex-wrap gap-8">
+		{#each icons as { name, color, link }}
+			<a class="oscilla rounded-lg ring-orange-200 outline-none focus:ring-2" href={link}>
+				<Icon {name} />
 			</a>
 		{/each}
 	</div>
@@ -109,38 +108,38 @@
 </div>
 
 <style>
-  .hero {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-    padding: 2rem 1rem;
-	 gap: 0.2rem;
-  }
+	.hero {
+		min-height: 100vh;
+		display: flex;
+		align-items: center;
+		padding: 2rem 1rem;
+		gap: 0.2rem;
+	}
 
-  .section-container {
-    display: grid;
-    grid-template-columns: 1fr;
-    align-items: stretch;
-    margin-left: auto;
-    margin-right: auto;
-    gap: 1rem;
-    padding: 2rem;
-    max-width: 80ch;
-  }
+	.section-container {
+		display: grid;
+		grid-template-columns: 1fr;
+		align-items: stretch;
+		margin-left: auto;
+		margin-right: auto;
+		gap: 1rem;
+		padding: 2rem;
+		max-width: 80ch;
+	}
 
-  @media (min-width: 45ch) {
-    .section-container {
-      grid-template-columns: 1fr 1fr;
-    }
-  }
+	@media (min-width: 45ch) {
+		.section-container {
+			grid-template-columns: 1fr 1fr;
+		}
+	}
 
-  @media (min-width: 63ch) {
-    .section-container {
-      grid-template-columns: repeat(3, 1fr);
-    }
+	@media (min-width: 63ch) {
+		.section-container {
+			grid-template-columns: repeat(3, 1fr);
+		}
 
-    #chan {
-      grid-row: span 2;
-    }
-  }
+		#chan {
+			grid-row: span 2;
+		}
+	}
 </style>
